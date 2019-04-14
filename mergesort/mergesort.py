9c412eb283f2474a3263f7f5ee29ad04e71f950a
bee5325 <tanyubin1993@hotmail.com>
2019-04-14 15:32:26 +0800

    Done with mergesort

--- mergesort/mergesort.py
+++ mergesort/mergesort.py
@@ -4,7 +4,7 @@ import copy
 
 size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
-numb_count = 20 
+numb_count = 600 
 rect_width = width // numb_count
 
 
@@ -20,19 +20,22 @@ class Number:
     def set_val(self, val):
         self.val = int(val)
         self.sur = pygame.Surface((rect_width, int(val)))
+        self.pos = (self.seq*rect_width, height-self.sur.get_height())
         self.sur.fill((255,255,255))
 
     def set_state(self, state):
-        if state == 0:
+        self.sur.set_alpha(255)
+        if state == 0: # checking
             self.sur.fill((255,255,255))
-        elif state == 1:
+        elif state == 1: # temporary placeholder
             self.sur.fill((0,255,0))
-        elif state == 2:
+            self.sur.set_alpha(125)
+        elif state == 2: # left and right
             self.sur.fill((0,0,255))
-        elif state == 3:
+        elif state == 3: # chosen
             self.sur.fill((255,0,0))
-        else:
-            self.sur.fill((50,50,50))
+        else: # inactive
+            self.sur.fill((80,80,80))
         self.draw()
 
     def set_seq(self, seq):
@@ -61,46 +64,59 @@ def game_setup():
 
 def merge_sort(nlist, templist, start, merge_size, left, right):
 
+    curr_idx = start + len(templist)
+    left_done = (left >= start+merge_size or left >= len(nlist))
+    right_done = (right >= start+2*merge_size or right >= len(nlist))
+    
     # both left and right exhausted
-    if left >= start+merge_size and right >= start+2*merge_size:
+    if left_done and right_done:
         for i in range(len(templist)):
             nlist[start+i] = copy.copy(templist[i])
         templist.clear()
         start = start + 2*merge_size
-        if start > len(nlist):
+        left = start
+        right = start + merge_size
+        if left >= len(nlist) or right >= len(nlist):
             start = 0
             merge_size *= 2
-        return start, merge_size, left, right
+            left = start
+            right = start + merge_size
+        if merge_size >= len(nlist):
+            return -1, -1, -1, -1, -1 
+        return start, merge_size, left, right, -1
 
     # one side is exhausted
-    if left >= start+merge_size or right >= start+2*merge_size:
-        if left >= start+merge_size:
-            templist.append(copy.copy(nlist[right]))
+    if left_done or right_done:
+        if left_done:
+            templist.append(Number(nlist[right].val, curr_idx))
+            chosen = right
             right += 1
-        elif right >= start+2*merge_size:
-            templist.append(copy.copy(nlist[left]))
+        elif right_done:
+            templist.append(Number(nlist[left].val, curr_idx))
+            chosen = left 
             left += 1
-        return start, merge_size, left, right
+        return start, merge_size, left, right, chosen
 
     # both sides haven't exhausted
     if nlist[left].val > nlist[right].val:
-        templist.append(copy.copy(nlist[left]))
-        left += 1
-    elif nlist[left].val <= nlist[right].val:
-        templist.append(copy.copy(nlist[right]))
+        templist.append(Number(nlist[right].val, curr_idx))
+        chosen = right 
         right += 1
-    return start, merge_size, left, right
+    else:
+        templist.append(Number(nlist[left].val, curr_idx))
+        chosen = left
+        left += 1
+    return start, merge_size, left, right, chosen
 
 
 def draw_list():
     screen.fill((0,0,0))
     for n in numbers:
         n.draw()
-    pygame.display.flip()
-    time.sleep(1)
 
 def draw_templist(templist):
     for n in templist:
+        n.set_state(1)
         n.draw()
 
 if __name__ == '__main__':
@@ -116,11 +132,28 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        for i, n in enumerate(numbers):
-            n.set_state(4) # inactive
+        if start != -1:
+            left_done = (left >= start+merge_size or left >= len(numbers))
+            right_done = (right >= start+2*merge_size or right >= len(numbers))
+            for i, n in enumerate(numbers):
+                if i == left or i == right:
+                    if i == left and not left_done:
+                        n.set_state(2)
+                    if i == right and not right_done:
+                        n.set_state(2)
+                elif i >= start and i < start+2*merge_size:
+                    n.set_state(0)
+                else:
+                    n.set_state(4) # inactive
 
-        print(start, left, right, [n.seq for n in templist])
-        start, merge_size, left, right = merge_sort(numbers, templist, start, merge_size, left, right)
+            start, merge_size, left, right, chosen = merge_sort(numbers, templist, start, merge_size, left, right)
 
-        draw_templist(templist)
+            if chosen != -1:
+                numbers[chosen].set_state(3)
+        else:
+            for n in numbers:
+                n.set_state(0)
+
         draw_list()
+        draw_templist(templist)
+        pygame.display.flip()
